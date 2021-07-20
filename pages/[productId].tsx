95a2587250e0f918a3b57db67fcf72c5b2cc4583
GeloUno <geloke59@gmail.com>
2021-07-20 15:15:48 +0200

    fallback blocking and if no data return 404

--- pages/[productId].tsx
+++ pages/[productId].tsx
@@ -15,20 +15,24 @@ interface ProductDetailsProps extends IContextProps {
 }
 
 
-interface IStaticPathParams extends ParsedUrlQuery {
+interface IStaticParams extends ParsedUrlQuery {
     productId: string
 }
+interface IStaticPaths extends ParsedUrlQuery {
+    productId: string | string[]
+}
+
 
 function ProductDetailPage(
     { product }: ProductDetailsProps
 ) {
 
-    // if fallback is block this in no needed
-    // if (!product) {
-    //     return (
-    //         <h1>Loading ...</h1>
-    //     )
-    // }
+    // if fallback is blocking in get static paths this in no needed
+    if (!product) {
+        return (
+            <h1>Loading ...</h1>
+        )
+    }
 
     return (
         <Fragment>
@@ -40,21 +44,32 @@ function ProductDetailPage(
 }
 
 
-export const getStaticProps: GetStaticProps<IContextProps, IStaticPathParams> = async ({ params }) => {
-
-    const paramProductId = params?.productId
-
+async function getDataProducts() {
     const filePath = path.join(process.cwd(), "data", 'dummy-backend.json')
 
     const fileReader = await fs.readFile(filePath)
 
     const data: IArrayProducts = await JSON.parse((fileReader).toString())
 
+    return data
+}
+
+export const getStaticProps: GetStaticProps<IContextProps, IStaticParams> = async ({ params }) => {
+
+    const paramProductId = params?.productId
+
+    const data = await getDataProducts()
 
 
     const product = data.products.find(element => {
         return (element.id === paramProductId)
     })
+
+    if (!product) {
+        return {
+            notFound: true
+        }
+    }
 
     return {
         props: {
@@ -64,11 +79,13 @@ export const getStaticProps: GetStaticProps<IContextProps, IStaticPathParams> = 
     }
 }
 
-export const getStaticPaths: GetStaticPaths<IStaticPathParams> = async () => {
+export const getStaticPaths: GetStaticPaths<IStaticPaths> = async () => {
+
+    const data = await getDataProducts()
+    const params: Array<{ params: { productId: string } }> = data.products.map((element) => { return { params: { productId: element.id } } })
+
     return {
-        paths: [
-            { params: { productId: 'p1' } }
-        ],
+        paths: params,
         fallback: 'blocking',
     }
 }

5cc333a566aabdf07bf4c1db7b2183312c44bbcb
GeloUno <geloke59@gmail.com>
2021-07-22 12:27:35 +0200

    SWR hook and get static props

--- pages/lastsales.tsx
+++ pages/lastsales.tsx
@@ -1,52 +1,87 @@
+import React, { useEffect, useState } from 'react'
 import useSWR from 'swr';
+import { GetStaticProps } from 'next';
 
-interface ISaleData {
+interface ISeleData {
     userName: string,
     value: number
 }
-
-const getDataArray = (dataparam: { [key: string]: { userName: string, value: number } }) => {
-
-    let data: Array<ISaleData> = [];
-
-    for (const key in dataparam) {
-        data.push({ userName: dataparam[key].userName, value: dataparam[key].value })
+interface ISelesData {
+    selses: Array<ISeleData>
+}
+interface ISelesUserDataServer {
+    [key: string]: {
+        userName: string, value: number
     }
+}
 
+const URL_SELES_USERS = 'https://nextclients-default-rtdb.europe-west1.firebasedatabase.app/sales.json'
+
+
+const fecher = async (url: string): Promise<ISelesUserDataServer> => {
+
+    const dataFache = await fetch(url)
+    const dataResponse: ISelesUserDataServer = await dataFache.json();
+
+    return dataResponse
+}
+
+
+const getArraySelesFromObjectSelesServer = (dataResponse: ISelesUserDataServer) => {
+
+    let data: Array<ISeleData> = [];
+
+    for (const key in dataResponse) {
+        data.push({ userName: dataResponse[key].userName, value: dataResponse[key].value })
+    }
     return data;
 }
 
-function LastSalesPage() {
-    const URL = 'https://nextclients-default-rtdb.europe-west1.firebasedatabase.app/sales.json'
-    const { data, error } = useSWR(URL)
 
-    if (data?.error) {
-        return (<h1>Error ...</h1>)
-    }
+function LastSalesPage({ selses }: ISelesData) {
 
-    if (!data) {
-        return (<h1>Loading ...</h1>)
-    }
 
-    if (data && !error) {
-        const dataUsers = getDataArray(data)
-        return (
-            <div>
-                {dataUsers.map((element) => (
-                    <div key={element.userName}>
-                        <h1>{element.userName}</h1>
-                        <p>{element.value}</p>
-                    </div>
-                ))}
-            </div>
-        )
-    }
+    const [selesData, setSelesData] = useState<ISeleData[]>(selses)
+
+    const { data, error } = useSWR(URL_SELES_USERS)
+
+    useEffect(() => {
+        if (data && !data?.error) {
+            const selesNewData = getArraySelesFromObjectSelesServer(data)
+            setSelesData(selesNewData)
+        }
+        return () => {
+            // cleanup
+        }
+    }, [data])
 
     return (
         <div>
-            <h1>no data</h1>
+            {selesData.map((element) => (
+                <div key={element.userName}>
+                    <h1>{element.userName}</h1>
+                    <p>{element.value}</p>
+                </div>
+            ))}
         </div>
-    );
-}
+    )
+};
 
 export default LastSalesPage;
+
+
+export const getStaticProps: GetStaticProps<ISelesData> = async () => {
+    console.log(`Regenerate`)
+
+    const dataSelesFromServer = await fecher(URL_SELES_USERS)
+
+    if (dataSelesFromServer?.error) {
+        return {
+            notFound: true
+        }
+    }
+    const data = getArraySelesFromObjectSelesServer(dataSelesFromServer)
+    return {
+        props: { selses: data },
+    }
+}
